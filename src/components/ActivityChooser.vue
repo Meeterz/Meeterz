16dff6384e9eebd2af8c7076892279193bcdcd7b
Tyson Sakamoto <tysonsakamoto@gmail.com>
2023-08-21 14:08:21 +0900

    inital commit with some files that dont work

---
+++ src/components/ActivityChooser.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <button @click="addAct">Add Activity</button>
+      <ul>
+        <template v-for="n in count">
+          <li>{{ n }}</li>
+        </template>
+      </ul>
+    </div>
+  </template>
+  
+<script>
+  import suggestion from './ActivitySuggestion.vue';
+  export default {
+    components: {
+      suggestion
+    }
+    methods: {
+      addAct() {
+        
+      }
+    }
+  }
+</script>
+  
---
+++ src/views/Room.vue
@@ -0,0 +1,21 @@
+<script>
+    import ActivityChooser from '../components/ActivityChooser.vue';
+    export default {
+        components: {
+            ActivityChooser
+        }
+    }
+</script>
+
+<template>
+    <head> 
+        <title>___'s room</title>
+    </head>
+    <body>
+        <h1>___'s room</h1>
+
+    <ActivityChooser/>
+        
+    </body>
+    
+</template>
