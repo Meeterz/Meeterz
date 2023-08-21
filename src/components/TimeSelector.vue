21f171fa825471c50eccf85ad3740b8a8a8b6055
Tyson Sakamoto <tysonsakamoto@gmail.com>
2023-08-22 00:24:22 +0900

    added time selector placeholder, and button to return to home page.

---
+++ src/components/TimeSelector.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+</script>
+
+<template>
+    <div class="box">
+        default dance
+    </div>
+</template>
+
+<style>
+    .box {
+        margin: 2rem;
+        border: solid 1pt black;
+    }
+</style>
--- src/views/Room.vue
+++ src/views/Room.vue
@@ -1,8 +1,10 @@
 <script>
     import ActivityChooser from '../components/ActivityChooser.vue';
+    import TimeSelector from '../components/TimeSelector.vue';
     export default {
         components: {
-            ActivityChooser
+            ActivityChooser,
+            TimeSelector,
         }
     }
 </script>
@@ -13,11 +15,39 @@
     </head>
     <body>
         <h1>___'s room</h1>
-    <div>
-        <h3>Activity Chooser</h3>
-         <ActivityChooser/>
-    </div>
+        <br>
+
+        <div class = 'options'>
+            <div class='showborder'>
+                <h3>Time Selecter</h3>
+                <TimeSelector/>
+            </div>
+
+            <div class='showborder'>
+                <h3>Activity Chooser</h3>
+                <ActivityChooser/>
+            </div>
+            <div class='showborder'>
+                <h3>Chatbox</h3>
+            </div>
+        </div>
+        <nav>
+            <RouterLink to="/"><button @click="navigate" role="link">leave</button></RouterLink>
+        </nav>
+
         
     </body>
     
 </template>
+
+<style>
+.options {
+    display: flex;
+    align-items: center;
+    padding: 2rem;
+}
+.showborder {
+    border: solid 1pt black;
+    padding: 1rem;
+}
+</style>
