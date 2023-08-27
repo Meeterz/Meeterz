ed71ee0438ca3a2d058715e1389e92ca5c80f30a
Chris Gray <cgray024@ucr.edu>
2023-08-27 17:36:13 +0900

    Added multiple timeslot functionality to calendar.
    Currently availabilities of one person can overlap, I'll be working on
    merging overlaps into one timeslot next

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -2,6 +2,7 @@
 import FullCalendar from '@fullcalendar/vue3'
 import resourceTimelinePlugin from '@fullcalendar/resource-timeline'
 import interactionPlugin from '@fullcalendar/interaction';
+import timeGridPlugin from '@fullcalendar/timegrid';
 
 export default {
   components: {
@@ -10,15 +11,35 @@ export default {
   data() {
     return {
       calendarOptions: {
-        plugins: [ resourceTimelinePlugin, interactionPlugin ],
+        plugins: [ resourceTimelinePlugin, interactionPlugin, timeGridPlugin],
+        initialView: 'timeGridWeek',
+        selectMirror: true,
         selectable: true,
         unselectAuto: false,
-        schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives'
-      }
+        schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
+        select: this.handleAvailSelect
+      },
+      eid: 0
+    }
+  },
+  methods: {
+    handleAvailSelect(selectInfo){
+      let calendarApi = selectInfo.view.calendar;
+
+      calendarApi.addEvent({
+        id: this.eid++,
+        title: 'available',
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      })
     }
   }
 }
 </script>
 <template>
+
   <FullCalendar :options="calendarOptions" />
 </template>
+
+
