9c66021e1e823dab34fea5fa9aecb834ce7163f1
Tyson Sakamoto <tysonsakamoto@gmail.com>
2023-08-31 00:06:14 +0900

    fized the acticity selecter to use db correctly

--- src/components/ActivityChooser.vue
+++ src/components/ActivityChooser.vue
@@ -1,11 +1,11 @@
 <template>
     <div>
       <!--temp SUGGESTER-->
-      <button @click="createActivity()">Add Activity</button>
+      <button @click="createActivity(), queryActs()">Add Activity</button>
       <input v-model="act" placeholder="Activity" />
       <ul>
-        <template v-for="n in count">
-          <li>{{ acts[n - 1] }}</li>
+        <template v-for="activity in acts">
+          <li>{{ activity.activity }}</li>
         </template>
       </ul>
 
@@ -13,8 +13,8 @@
       <div>Selected: {{ selected }}</div>
       <select v-model="selected">
         <option disabled value="">Please select one</option>
-        <template v-for="n in count">
-          <option>{{ acts[n - 1] }}</option>
+        <template v-for="activity in acts">
+          <option>{{activity.activity}}</option>
         </template>
       </select>
     </div>
@@ -35,51 +35,65 @@ import { db } from '../firebaseResources';
     deleteDoc,
     } from 'firebase/firestore'
 
-  //maybe turn each suggestion to a seperate component, but it didtn work so idk
-  //import suggestion from './ActivitySuggestion.vue'; 
+
   export default {
-    /*  I dont know how props work
-    props: {
-      message: {
-        type: String,
-        default() {
-          return 'Activity ' + count; 
-        }
-      }  
-    },  */
     
-    components: {
-      //suggestion
-    },  
     data() {
       return {
         count:0, 
         act: '',
         acts: [],
       }
-
     },
+
+    mounted() {
+      this.queryActs();
+    },
+
     methods: {
       async createActivity() { //adds an activity to the room. 
         try{
           console.log('Adding an Activity');
-          this.count += 1;
-          this.acts.push(this.act);
           console.log(this.act);
           
           const docRef = await addDoc(
             collection(db, 'rooms', this.$route.params.code, 'activities'),
             {
-              activity: this.acts,
+              activity: this.act,
             }
           );
-        }
 
+          console.log('Completed createActivity')
+        }
        catch(err){
         console.error(err);
         }
       },
+
+      async queryActs() {
+        try {
+          console.log('Calling queryActs')
+          this.acts = [];
+          let q = query(collection(db,'rooms', this.$route.params.code, 'activities'));
+          const queryResponse = await getDocs(q);
+
+          queryResponse.forEach((responseItem) => {
+          console.log({ id: responseItem.id, data: responseItem.data() });
+          this.acts.push({
+            id: responseItem.id,
+            ...responseItem.data(),
+          });
+        });
+
+        console.log('Completed queryActs');
+        }
+        catch(err) {
+          console.error('Error in queryActs', err);
+        }
+      }
+
     }
+
   }
 </script>
   
