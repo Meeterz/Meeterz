555edc8490ce87f9d6ed1d0c42b31286e78dbfbe
Chris Gray <cgray024@ucr.edu>
2023-08-30 09:51:14 +0900

    Revert "Overlapping availabilities now auto merge"
    This reverts commit 3d21bff1d423ebe2ec11c7624b1c250b7dff09f4.
    Accidentally pushed to main -- LOL

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -16,8 +16,6 @@ export default {
         selectMirror: true,
         selectable: true,
         unselectAuto: false,
-        eventOverlap: false,
-        selectOverlap: true,
         schedulerLicenseKey: 'CC-Attribution-NonCommercial-NoDerivatives',
         select: this.handleAvailSelect
       },
@@ -29,43 +27,21 @@ export default {
       let calendarApi = selectInfo.view.calendar;
 
       calendarApi.unselect();
-      var event = {
+
+      calendarApi.addEvent({
         id: this.eid++,
         title: 'available',
         start: selectInfo.startStr,
         end: selectInfo.endStr,
         allDay: selectInfo.allDay
-      };
-      //returns false if there's no overlap, but otherwise will return an event id of an overlapping event
-      var overlap = this.mergeOverlap(event, calendarApi);
-      console.log(overlap);
-      if(overlap === false){
-        calendarApi.addEvent(event);
-      }
-      else{
-        calendarApi.getEventById(overlap).remove();
-        calendarApi.addEvent(event);
-
-      }
-    },
-    mergeOverlap(event, calendar){
-    // "calendar" on line below should ref the element on which fc has been called 
-    var array = calendar.getEvents();
-    for(let i in array){
-        if (Date.parse(event.end) >= Date.parse(array[i].start) && Date.parse(event.start) <= Date.parse(array[i].end)){
-          event.start = Date.parse(event.start) < Date.parse(array[i].start) ? event.start : array[i].start;
-          event.end = Date.parse(event.end) > Date.parse(array[i].end) ? event.end : array[i].end;
-          return array[i].id;
-        }
+      })
     }
-    return false;
-}
   }
 }
 </script>
 <template>
 
-  <FullCalendar :options="calendarOptions" :eventOverlap = false />
+  <FullCalendar :options="calendarOptions" />
 </template>
 
 
